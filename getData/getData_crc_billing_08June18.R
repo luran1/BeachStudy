63e8cfcf8fabf2ef797ace7d5460131c18aac97d
dlemas <djlemas@ufl.edu>
2018-06-08 23:40:31 -0400

    first commit

---
+++ getData/getData_crc_billing_08June18.R
@@ -0,0 +1,95 @@
+##-------------- 
+# **************************************************************************** #
+# ***************                Project Overview              *************** #
+# **************************************************************************** #
+
+# Author:      Dominick Lemas 
+# Date:        June 08, 2018 
+# IRB:
+# Description: Data management for CTSI/CRC billing and services data extracted from RedCap. 
+# Data: C:\Users\djlemas\Dropbox (UFL)\02_Projects\BEACH_STUDY\RedCap
+# Obj: Format data and basic analysis.
+
+# **************************************************************************** #
+# ***************                Directory Variables           *************** #
+# **************************************************************************** #
+
+# Computer
+# location="djlemas";location
+# location="Dominick";location
+
+# Directory Locations
+work.dir=paste("C:\\Users\\",location,"\\Dropbox (UFL)\\02_Projects\\BEACH_STUDY\\RedCap\\ALL_DATA\\",sep="");work.dir
+data.dir=paste("C:\\Users\\",location,"\\Dropbox (UFL)\\02_Projects\\BEACH_STUDY\\RedCap\\ALL_DATA\\",sep="");data.dir
+out.dir=paste("C:\\Users\\",location,"\\Dropbox (UFL)\\02_Projects\\BEACH_STUDY\\RedCap\\tables\\",sep="");out.dir
+
+# Set Working Directory
+setwd(work.dir)
+list.files()
+
+# **************************************************************************** #
+# ***************                Library                       *************** #
+# **************************************************************************** #
+
+# library(readxl)
+library(data.table)
+library(tidyr)
+library(dplyr)
+library(reshape2)
+
+# **************************************************************************** #
+# ***************  TheBreastfeedingAndE_BILLING_DATA_2018-06-08_1318.csv                                              
+# **************************************************************************** # 
+
+#Read Data
+data.file.name="TheBreastfeedingAndE_BILLING_DATA_2018-06-08_1318.csv";data.file.name
+data.file.path=paste0(data.dir,"\\",data.file.name);data.file.path
+billing<- read.csv(data.file.path);ufhealth.abx
+
+# look at data
+dat=billing
+head(dat); str(dat); names(dat)
+
+# format variables
+df <- dat %>% 
+  mutate(redcap_event_name = factor(redcap_event_name, 
+                                    levels = c("third_trimester_arm_1", 
+                                               "two_week_arm_1", 
+                                               "two_month_arm_1")))
+
+# drop NA observations
+dat.s=df %>%
+  na.omit() %>%
+  group_by(test_id, redcap_event_name) %>%
+  arrange(crc_date_of_service) 
+
+# how much per visit/participant?
+test=dat.s %>%
+  group_by(test_id, redcap_event_name) %>%
+  summarize(count=n_distinct(test_id),
+            bill_mean=mean(crc_amount_due, na.rm=T),
+            bill_sum=sum(crc_amount_due))
+test %>%
+  group_by(redcap_event_name) %>%
+  summarize(mean(bill_sum),
+            min(bill_sum),
+            max(bill_sum))
+
+# how much per visit?
+dat.s %>%
+  group_by(redcap_event_name) %>%
+  summarize(count=n_distinct(test_id),
+            bill_mean=mean(crc_amount_due, na.rm=T),
+            bill_sum=sum(crc_amount_due))
+
+# 3rd  $100
+# 2wk  $160
+# 2mo  $160
+# 12mo $160
+
+# CRC= $640
+# Inc= $160
+# Part= $800
+
+80*800= $64,000
+
