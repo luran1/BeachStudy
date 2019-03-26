b6cf066a5c47ecc9427d612928127d797bb0c307
Dominick J. Lemas <dominicklemas@gmail.com>
2019-03-26 18:14:57 -0400

    interview table 1. drafted. need to add study group

--- getData/getData_beach-interview_redcapR.R
+++ getData/getData_beach-interview_redcapR.R
@@ -43,13 +43,22 @@ exportFieldNames(rcon)
 # Error: $ operator is invalid for atomic vectors
 # https://github.com/nutterb/redcapAPI/issues/126
 
-desired_records_v1 <- c("BIS001A")
-desired_fields_v1 <- c("record_id", "int_consent_date")
+desired_records_v1 <- c("BIS001A","BIS002A","BIS003A","BIS004A",											
+                        "BIS005A","BIS006A","BIS007A","BIS008A","BIS009A","BIS010A","BIS011A","BIS013A",
+                        "BIS014A","BIS018A","BIS019A","BIS022A","BIS023A","BIS024A","BIS025A","BIS026A",
+                        "BIS027A","BIS028A","BIS029A","BIS030A","BIS031A","BIS032A","BIS033A","BIS034A",
+                        "PRG001","PRG002","PRG003","PRG004","PRG005","PRG006","PRG007","PRG008","PRG009",
+                        "PRG010","PRG011","PRG012","PRG013","PRG014","PRG015","PRG016","PRG017","PRG018",
+                        "PRG020")	
+
+desired_fields_v1 <- c("record_id", "int_consent_date","interphone_prepreg_bmi","interphone_age","int_audio_length_min",
+                       "mompa_walk_slow","mompa_walk_quick", "mompa_walk_hills", "mompa_jog", "mompa_prenatal_exer", "mompa_swim",
+                       "mompa_dance")
 
 ds_some_rows_v1 <- redcap_read(
   redcap_uri = uri, 
   token      = interview_token, 
   records    = desired_records_v1,
   fields     = desired_fields_v1
-)$data
+  )$data
 
