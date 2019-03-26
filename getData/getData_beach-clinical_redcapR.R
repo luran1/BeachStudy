9cbadf95856b412aaf4974f061c9f19ea0f1fc15
Dominick J. Lemas <dominicklemas@gmail.com>
2019-03-25 21:44:26 -0400

    pulling data from redcap

--- getData/getData_beach-clinical_redcapR.R
+++ getData/getData_beach-clinical_redcapR.R
@@ -14,6 +14,7 @@
 
 library(keyringr)
 library(redcapAPI)
+library(REDCapR)
 
 # Get Redcap API Token
 # # https://cran.r-project.org/web/packages/keyringr/vignettes/Avoiding_plain_text_passwords_in_R_with_keyringr.html
@@ -28,3 +29,20 @@ rcon <- redcapConnection(url=uri, token=beach_token)
 
 # list of instruments
 exportInstruments(rcon)
+
+# list of events
+exportEvents(rcon)
+
+# list records
+exportRecords(rcon)
+
+desired_records_v1 <- c("BLS001A")
+desired_fields_v1 <- c("test_id", "redcap_event_name", "mom3t_breast_surg")
+
+ds_some_rows_v1 <- redcap_read(
+  redcap_uri = uri, 
+  token      = beach_token, 
+  records    = desired_records_v1,
+  fields     = desired_fields_v1
+)$data
+
