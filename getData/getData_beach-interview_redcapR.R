6485d4859e3e14e5aea48c9c2853d9e8d2b00d36
Dominick J. Lemas <dominicklemas@gmail.com>
2019-03-27 15:00:07 -0400

    beach-interview data pull drafted

--- getData/getData_beach-interview_redcapR.R
+++ getData/getData_beach-interview_redcapR.R
@@ -16,6 +16,7 @@ library(keyringr)
 library(redcapAPI)
 library(REDCapR)
 
+# Login to Gatorlink VPN
 
 # Get Redcap API Token
 # # https://cran.r-project.org/web/packages/keyringr/vignettes/Avoiding_plain_text_passwords_in_R_with_keyringr.html
@@ -40,25 +41,26 @@ exportRecords(rcon)
 # export field names
 exportFieldNames(rcon)
 
-# Error: $ operator is invalid for atomic vectors
-# https://github.com/nutterb/redcapAPI/issues/126
+# Variables
+desired_fields_v1 <- c("record_id","int_study_grp","interphone_date","int_consent_date","int_consent_complete",
+                       "int_interview_date","int_audio_length_min","int_complete", # study 
+                       "interphone_prepreg_bmi","interphone_age","mom3t_prepreg_bmi",
+                       "mompa_walk_slow","mompa_walk_quick", "mompa_walk_hills", "mompa_jog", "mompa_prenatal_exer", "mompa_swim","mompa_dance", # physical activity
+                       "int_guide_education","int_guide_employmnt","int_guide_occupation"
+                       )
 
-desired_records_v1 <- c("BIS001A","BIS002A","BIS003A","BIS004A",											
-                        "BIS005A","BIS006A","BIS007A","BIS008A","BIS009A","BIS010A","BIS011A","BIS013A",
-                        "BIS014A","BIS018A","BIS019A","BIS022A","BIS023A","BIS024A","BIS025A","BIS026A",
-                        "BIS027A","BIS028A","BIS029A","BIS030A","BIS031A","BIS032A","BIS033A","BIS034A",
-                        "PRG001","PRG002","PRG003","PRG004","PRG005","PRG006","PRG007","PRG008","PRG009",
-                        "PRG010","PRG011","PRG012","PRG013","PRG014","PRG015","PRG016","PRG017","PRG018",
-                        "PRG020")	
-
-desired_fields_v1 <- c("record_id", "int_consent_date","interphone_prepreg_bmi","interphone_age","int_audio_length_min",
-                       "mompa_walk_slow","mompa_walk_quick", "mompa_walk_hills", "mompa_jog", "mompa_prenatal_exer", "mompa_swim",
-                       "mompa_dance")
-
+# pull data
 ds_some_rows_v1 <- redcap_read(
+  batch_size=150L,
   redcap_uri = uri, 
   token      = interview_token, 
-  records    = desired_records_v1,
   fields     = desired_fields_v1
   )$data
+
+# chcek data pull
+str(ds_some_rows_v1)
+ds_some_rows_v1[1]
+
+# ready for some data analysis!
+
 
